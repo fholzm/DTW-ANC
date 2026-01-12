0afd8484e9d59ae49cb6ddcfddace1a9c26b12c3
Felix Holzmüller <f.holzmueller@kug.ac.at>
2026-01-12 14:52:08 +0100

    Implement interpolation

--- prototype.py
+++ prototype.py
@@ -3,9 +3,16 @@ import numpy as np
 import dtw
 import sofar as sf
 import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline as ip
+
+
+# %% functions
+def system_mismatch(ir_ref, ir_query):
+    return np.linalg.norm(ir_ref - ir_query) / np.linalg.norm(ir_ref)
+
 
 # %% Load and inspect HRIR
-hrir = sf.read_sofa("HRIRs/scut/SCUT_KEMAR_radius_1.sofa")
+hrir = sf.read_sofa("HRIRs/scut/SCUT_KEMAR_radius_0.25.sofa")
 hrir.inspect()
 
 # %%
@@ -62,11 +69,13 @@ for i in range(Z.shape[0]):
 plt.grid()
 
 # %% Calculate DTW between two HRIRs
-ir_idx1 = np.where((hrir_angles == 250))[0][0]
-ir_idx2 = ir_idx1 + 2
+ir_idx1 = np.where((hrir_angles == 270))[0][0]
+ir_idx2 = ir_idx1 + 4
 
-query = np.squeeze(hrir_data_horizon[ir_idx1, 0, 50:150])
-reference = np.squeeze(hrir_data_horizon[ir_idx2, 0, 50:150])
+query = np.squeeze(hrir_data_horizon[ir_idx1, 0, 0:150])
+reference = np.squeeze(hrir_data_horizon[ir_idx2, 0, 0:150])
+
+target = np.squeeze(hrir_data_horizon[ir_idx1 + 2, 0, 0:150])
 
 # stepPattern = dtw.rabinerJuangStepPattern(6, "c")
 stepPattern = dtw.symmetricP2
@@ -91,9 +100,39 @@ plt.plot(query[wq], label="Warped Query", color="tab:blue", linestyle="--")
 plt.title("Warping query")
 plt.legend()
 
+# plt.figure()
+# plt.plot(reference, label="Reference", color="tab:blue")
+# plt.plot(query, label="Query", color="tab:orange")
+# plt.plot(reference[wt], label="Warped Reference", color="tab:orange", linestyle="--")
+# plt.title("Warping reference")
+# plt.legend()
+
+# %% Inteprolation
+# Linear interpolation of coeffs
+h_interp_warped = (reference + query[wq]) / 2
+h_interp_direct = (query + reference) / 2
+
+# Find updated indoces for de-warping
+displacement = np.arange(len(query)) - wq
+idx_dewarping = np.arange(len(query)) - displacement * 0.5
+
+# %% Apply spline interpolation to get samples at integer-indices
+spline_wapred = ip(idx_dewarping, h_interp_warped)
+h_dtw_dewarped = spline_wapred(np.arange(len(h_interp_warped)))
+
+# %% Plot reference vs inteprolated
+D_direct = system_mismatch(target, h_interp_direct)
+D_dtw = system_mismatch(target, h_dtw_dewarped)
+D_ref = system_mismatch(target, target)
+
 plt.figure()
-plt.plot(reference, label="Reference", color="tab:blue")
-plt.plot(query, label="Query", color="tab:orange")
-plt.plot(reference[wt], label="Warped Reference", color="tab:orange", linestyle="--")
-plt.title("Warping reference")
+plt.plot(target, label=f"Reference (D={D_ref:.2f})", color="tab:blue")
+plt.plot(h_dtw_dewarped, label=f"DTW Interpolated (D={D_dtw:.2f})", color="tab:orange")
+plt.plot(
+    h_interp_direct, label=f"Direct Interpolated (D={D_direct:.2f})", color="tab:green"
+)
 plt.legend()
+plt.grid()
+
+# %%
+plt.show()
