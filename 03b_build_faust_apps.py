565fcd388372401089ece43dfd6898f519635df3
Felix Holzmüller <f.holzmueller@kug.ac.at>
2026-01-29 14:51:26 +0100

    Enable OSC support for faust apps

--- 03b_build_faust_apps.py
+++ 03b_build_faust_apps.py
@@ -3,10 +3,19 @@ import subprocess
 
 def main():
     subprocess.run(
-        ["faust2jack", "-double", "anc_simulation/faust_dsp/sec_path_interp.dsp"]
+        [
+            "faust2jack",
+            "-double",
+            "-osc",
+            "anc_simulation/faust_dsp/sec_path_interp.dsp",
+        ]
     )
-    subprocess.run(["faust2jaqt", "-double", "anc_simulation/faust_dsp/fxlms.dsp"])
-    subprocess.run(["faust2jaqt", "-double", "anc_simulation/faust_dsp/noise.dsp"])
+    subprocess.run(
+        ["faust2jaqt", "-double", "-osc", "anc_simulation/faust_dsp/fxlms.dsp"]
+    )
+    subprocess.run(
+        ["faust2jaqt", "-double", "-osc", "anc_simulation/faust_dsp/noise.dsp"]
+    )
 
 
 if __name__ == "__main__":
